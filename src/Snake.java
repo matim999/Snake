ba39d69e483754d4363bee6f43004d4c9f4bd224
Mateusz Maciejko <Mateusz.Maciejko@comarch.pl>
2018-12-03 12:24:15 +0100

    test5

--- src/Snake.java
+++ src/Snake.java
@@ -7,7 +7,7 @@ import javax.swing.JComponent;
 public class Snake extends JComponent
 {
 	/**
-	 * ab
+	 * abbbb
 	 */
 	private static final long serialVersionUID = 1L;
 	private static SnakeElement last;
